1b57a1598e06b9d0f5f762edccdc439ca2687a47
Paulo <jaccoller@gmail.com>
2024-04-20 20:19:20 -0300

    Login v1.0

---
+++ login/script.js
@@ -0,0 +1,28 @@
+function login() {
+    let username = 'Paulo'
+    let password = 'x45b150z'
+    let userV = document.getElementById('input-user')
+    let passV = document.getElementById('input-pass')
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('login-img')
+    if (userV.value.length == 0 || passV.value.length == 0) {
+        msg.innerHTML = `Preencha todos os campos acima!`
+        img.style.color = '#ffbe4d'
+    } else if (userV.value == username && passV.value == password) {
+        msg.innerHTML = `Logado com sucesso!`
+        img.style.color = '#39d47c'
+    } else {
+        msg.innerHTML = `Usuário ou senha incorretos!`
+        img.style.color = '#ff4d53'
+    }
+}
+
+function color1() {
+    let button = document.getElementById('button-access')
+    button.style.backgroundColor = '#39d47c'
+}
+
+function color2() {
+    let button = document.getElementById('button-access')
+    button.style.backgroundColor = '#1e996a'
+}
